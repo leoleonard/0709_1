d756de7c7f8ddf1e78483e351c0b711cf864f1a1
Damian Juzyk <damian.juzyk@gmail.com>
2018-09-09 13:22:30 +0200

    add check gitignore

--- src/Task1.java
+++ src/Task1.java
@@ -5,5 +5,7 @@ public class Task1 {
         System.out.println("programowania");
         System.out.println("i");
         System.out.println("commitowania");
+        System.out.println("sprawdzenie .gitignore");
+
     }
 }
